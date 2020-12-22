96982ef4239384cd820ea002018e327fcaa14c4c
unknowhaker <70791877+unknowhaker@users.noreply.github.com>
2020-12-22 16:05:45 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,26 @@
+class Rope {
+    constructor(bodyA,bodyB,offsetX,offsetY){
+        this.offsetX=offsetX;
+        this.offsetY=offsetY;
+        var option={
+            //bodyA:body1,
+            //bodyB:body2,
+            //pointB{x:this.offsetX, y:this.offsetY};
+            bodyA:bodyA,
+            bodyB:bodyB,
+            pointB: {x:this.offsetX, y:this.offsetY
+        }
+        this.chain=Constraint.create(option);
+        World.add(world,this.chain);
+    }
+    display(){
+        var possA=this.chain.bodyA.position;
+        var possB=this.chain.bodyB.position;
+        push();
+        strokeWeight(3);
+        //line (possA.x,possA.y,possB.x,possB.y);
+        //pop();
+        line (possA.x,possA.y,possB.x+offsetX,possB.y+offsetY);
+        pop();
+    }
+}
